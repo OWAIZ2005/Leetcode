aace7a0e345647620127509907b7432eed9521fb
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-01-20 23:30:28 +0530

    Updated commit

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int reverse(int n)
+    {
+        int revNum = 0;
+        while (n != 0)
+        {
+            int digit = n % 10;
+            n = n / 10;
+
+            if (revNum > INT_MAX / 10 || (revNum == INT_MAX / 10 && digit > 7))
+                return 0;
+            if (revNum < INT_MIN / 10 || (revNum == INT_MIN / 10 && digit < -8))
+                return 0;
+
+            revNum = revNum * 10 + digit;
+        }
+        return revNum;
+    }
+};
