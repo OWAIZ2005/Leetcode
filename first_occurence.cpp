2d770dba8da55be315c96ffccd9ce377cc72d1e9
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-02-10 21:51:46 +0530

    brute force

--- first_occurence.cpp
+++ first_occurence.cpp
@@ -3,47 +3,25 @@ class Solution
 public:
     int strStr(string haystack, string needle)
     {
-        if (needle.empty())
-            return 0;
-
-        int n = haystack.size(), m = needle.size();
-        vector<int> lps(m, 0);
-
-        // Build LPS array
-        for (int i = 1, len = 0; i < m;)
+        for (int i = 0; i < haystack.size(); i++)
         {
-            if (needle[i] == needle[len])
+            if (haystack[i] == needle[0])
             {
-                lps[i++] = ++len;
-            }
-            else if (len > 0)
-            {
-                len = lps[len - 1];
-            }
-            else
-            {
-                lps[i++] = 0;
+                int j = 0; // for traversing the needle
+                int k = i; // for traversing the haystack
+
+                while (j < needle.size() && k < haystack.size() && needle[j] == haystack[k])
+                {
+                    j++;
+                    k++;
+                    if (j == needle.size()) // condition to check if we reacched the final character of the needle or not
+                    {
+                        return i;
+                    }
+                }
             }
         }
 
-        // KMP search
-        for (int i = 0, j = 0; i < n;)
-        {
-            if (haystack[i] == needle[j])
-            {
-                i++;
-                j++;
-            }
-            if (j == m)
-                return i - j;
-            else if (i < n && haystack[i] != needle[j])
-            {
-                if (j > 0)
-                    j = lps[j - 1];
-                else
-                    i++;
-            }
-        }
         return -1;
     }
 };
