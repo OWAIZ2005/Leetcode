2111a7de5fe0eeb5b9903d5a1b8a85160ea5fb2f
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-02-23 23:23:54 +0530

    best approch - not optimal approch

--- longest_consecutive_sequence.cpp
+++ longest_consecutive_sequence.cpp
@@ -1,30 +1,28 @@
-class Solution
-{
+class Solution {
 public:
-    int longestConsecutive(vector<int> &nums)
-    {
-        sort(nums.begin(), nums.end());
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-        int lastSmaller = INT_MIN;
-        int count = 0;
-        int longest = 0;
-        for (int i = 0; i < n; i++)
+        int lastSmaller = INT_MIN;    // declare this variable to store the last minimum found value 
+        int count = 0 ;
+        int longest = 0;                    // initiate this variable to find have a count of the element in the longest consequtive sequence 
+        for(int i =0 ; i<n;i++)
         {
-            if (nums[i] - 1 == lastSmaller)
+            if(nums[i]-1 == lastSmaller)
             {
-                count++;
-                lastSmaller = nums[i];
+                count ++;
+                lastSmaller=nums[i];
             }
 
-            else if (nums[i] != lastSmaller)
+            else if (nums[i]!=lastSmaller)
             {
                 count = 1;
                 lastSmaller = nums[i];
             }
 
-            longest = max(longest, count);
+            longest = max(longest,count);
         }
 
-        return longest;
+        return longest;    
     }
 };
