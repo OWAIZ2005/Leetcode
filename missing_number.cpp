d4e23e12a98f3282a7477f7088d34ef450613698
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-03-14 20:50:30 +0530

    initial commit

--- missing_number.cpp
+++ missing_number.cpp
@@ -4,7 +4,7 @@ public:
     int missingNumber(vector<int> &nums)
     {
         int n = nums.size();
-        int expected = n * (n + 1) / 2;
+        int expected = n * (n + 1) / 2; // formula for sum of n numbers
         int actual = 0;
         for (int i = 0; i < n; i++)
         {
