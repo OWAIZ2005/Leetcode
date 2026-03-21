0a54902be08347feb57612c0074095e93c102f61
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-03-21 19:11:13 +0530

    initial commit

---
+++ koko_eating_bananas.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int findMax(vector<int> &piles)
+    {
+        int maximum = INT_MIN;
+        int n = piles.size();
+        for (int i = 0; i < n; i++)
+        {
+            maximum = max(maximum, piles[i]);
+        }
+        return maximum;
+    }
+    long long calculateTotalHour(vector<int> &piles, int hourly)
+    {
+        long long totalH = 0;
+        int n = piles.size();
+        for (int i = 0; i < n; i++)
+        {
+            totalH += (piles[i] + hourly - 1) / hourly;
+        }
+        return totalH;
+    }
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+        int low = 1;
+        int high = findMax(piles);
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            long long totalH = calculateTotalHour(piles, mid);
+            if (totalH <= h)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
