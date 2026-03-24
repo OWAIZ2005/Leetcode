50550694dba3df4d9c9f8fe262f1d2f4b72cc0f9
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-03-25 00:38:04 +0530

    initial commit

---
+++ capacity_to_shift_packages.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+public:
+    int capacitycalculation(vector<int> &weights, int capacity)
+    {
+        int n = weights.size();
+        int days_used = 1;
+        int current_sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if ((current_sum + weights[i]) <= capacity)
+            {
+                current_sum += weights[i];
+            }
+            else
+            {
+                days_used++;
+                current_sum = weights[i];
+            }
+        }
+        return days_used;
+    }
+    int shipWithinDays(vector<int> &weights, int days)
+    {
+        int n = weights.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += weights[i];
+        }
+        int maxi = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            maxi = max(maxi, weights[i]);
+        }
+
+        int low = maxi;
+        int high = sum;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (capacitycalculation(weights, mid) > days)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
