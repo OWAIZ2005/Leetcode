9891dd54c399d8c5e0e5bc3d46a5df99f4ea5232
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-03-25 23:31:16 +0530

    initial commit

--- capacity_to_shift_packages.cpp
+++ capacity_to_shift_packages.cpp
@@ -1,14 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    int capacitycalculation(vector<int> &weights, int capacity)
+    int capacitycalculation(vector<int>& weights , int capacity)  // so the main idea should be the sum should be less than the capacity 
     {
         int n = weights.size();
         int days_used = 1;
         int current_sum = 0;
-        for (int i = 0; i < n; i++)
+        for(int i =0;i<n;i++)
         {
-            if ((current_sum + weights[i]) <= capacity)
+            if((current_sum + weights[i]) <= capacity)
             {
                 current_sum += weights[i];
             }
@@ -16,38 +15,44 @@ public:
             {
                 days_used++;
                 current_sum = weights[i];
+
             }
         }
         return days_used;
+
     }
-    int shipWithinDays(vector<int> &weights, int days)
-    {
+    int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
-        int sum = 0;
-        for (int i = 0; i < n; i++)
+        int sum = 0 ;
+        for(int i =0;i<n;i++)
         {
-            sum += weights[i];
+            sum+=weights[i];
         }
         int maxi = INT_MIN;
-        for (int i = 0; i < n; i++)
+        for(int i =0;i<n;i++)
         {
-            maxi = max(maxi, weights[i]);
+            maxi =  max(maxi,weights[i]);
         }
 
         int low = maxi;
         int high = sum;
-        while (low <= high)
+        while(low<=high)
         {
-            int mid = (low + high) / 2;
-            if (capacitycalculation(weights, mid) > days)
+            int mid = (low+high)/2;
+            if(capacitycalculation(weights,mid) > days)
             {
-                low = mid + 1;
+                low=mid+1;
+
             }
             else
             {
-                high = mid - 1;
+                high = mid-1;
             }
         }
         return low;
+
+
+
+        
     }
 };
