8147bcb0819a14a75d919230bac9a5807585dfe5
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-04-01 23:12:37 +0530

    initial commit

--- largest_odd_number_in_a_string.cpp
+++ largest_odd_number_in_a_string.cpp
@@ -1,22 +1,21 @@
-class Solution
-{
+class Solution {
 public:
-    string largestOddNumber(string num)
-    {
+    string largestOddNumber(string num) {    // so we basically do npt need a single largest odd number , we have to find a substring 
         int n = num.size();
-
-        if ((num[n - 1] - '0') % 2 != 0)
-            return num;
-        for (int i = n - 1; i >= 0; i--)
+        
+        if((num[n-1]-'0')%2!=0) return num;
+        for(int i = n-1;i>=0;i--)
         {
-            if ((num[i] - '0') % 2 != 0)
+            if((num[i]-'0') % 2 != 0)
             {
-                return num.substr(0, i + 1);
+                return num.substr(0,i+1);
             }
         }
         return "";
+        
     }
 };
 
-// so in other to convert a string like 52 to a number u can do num[i]-'0'
-// and also substring can be returned by num.substr(start ,end);
+
+//so in other to convert a string like 52 to a number u can do num[i]-'0' 
+//and also substring can be returned by num.substr(start ,end);
