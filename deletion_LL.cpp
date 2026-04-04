d9684cf7588905829a0d5fb964d0f61ffa2131d6
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-04-04 22:01:37 +0530

    initial commit

---
+++ deletion_LL.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+Node *convertArr2LL(vector<int> &arr)
+{
+    if (arr.size() == 0)
+        return NULL;
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+Node *removesHead(Node *head)
+{
+    if (head == NULL)
+        return head;
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+Node *removeTail(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return false;
+    }
+    Node *temp = head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+
+Node *deleteK(Node *head, int k)
+{
+    if (head == NULL)
+        return head;
+    if (k == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    int counter = 0;
+    Node *temp = head;
+    Node *prev = NULL;
+    while (temp != NULL)
+    {
+        counter++;
+        if (counter == k)
+        {
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main()
+{
+    vector<int> arr = {2, 5, 8, 7};
+    Node *head = convertArr2LL(arr);
+    // head = removesHead(head);
+    // head = removeTail(head);
+    head = deleteK(head, 8);
+    print(head);
+}
