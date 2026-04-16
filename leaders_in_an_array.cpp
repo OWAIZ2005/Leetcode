63df25d11f7f8ff104e32601b4d185eb7f015825
OWAIZ2005 <mohamedbasheer69770105@gmail.com>
2026-04-16 16:51:43 +0530

    initial commit

---
+++ leaders_in_an_array.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> replaceElements(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> result(n);
+
+        int maxi = -1;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            result[i] = maxi;
+            maxi = max(maxi, arr[i]);
+        }
+
+        return result;
+    }
+};
